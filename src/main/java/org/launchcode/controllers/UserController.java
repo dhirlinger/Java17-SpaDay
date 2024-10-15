9381099cdf19509b93472ada23181b84ee2ee2d6
Doug Hirlinger <doughirlinger@gmail.com>
2024-10-14 21:45:38 -0400

    form handling needs fixing

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,9 +14,22 @@ public class UserController {
 
     //points to /user/add
     @GetMapping("add")
-    public String displayAddUserForm(Model model){
-        model.addAttribute(new User());
+    public String displayAddUserForm(){
     return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        //model.addAttribute(user);
+
+
+        if (user.getUsername().equals(verify)) {
+            String message = "Welcome " + user.getUsername();
+            model.addAttribute("message", message);
+            return "user/index";
+        } else {
+            return "user/add";
+        }
     }
 
 
