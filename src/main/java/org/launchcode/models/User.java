ec19e70444b2a21807479267e652fb593dada707
Doug Hirlinger <doughirlinger@gmail.com>
2024-10-14 20:33:21 -0400

    ch 15 studio start

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,17 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @NotBlank
+    private String verify;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String verify, String email, String password) {
         this();
         this.username = username;
+        this.verify = verify;
         this.email = email;
         this.password = password;
     }
@@ -22,6 +37,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public @NotBlank String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(@NotBlank String verify) {
+        this.verify = verify;
     }
 
     public String getEmail() {
