cb5f910c10ce4bd3e18031177fe5f94e0d168ebb
Doug Hirlinger <doughirlinger@gmail.com>
2024-10-16 09:23:38 -0400

    first commit since start over

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -10,30 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
 
-    //points to /user/add
-    @GetMapping("add")
-    public String displayAddUserForm(){
-    return "user/add";
-    }
-
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        //model.addAttribute(user);
-
-
-        if (user.getPassword().equals(verify)) {
-            String message = "Welcome " + user.getUsername() +".";
-            model.addAttribute("message", message);
-            return "user/index";
-        } else {
-            model.addAttribute("error", "Password verification did not match password.");
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute("email", user.getEmail());
-            return "user/add";
-        }
+    public String displayAddUserForm(){
+        return "add";
     }
 
 
